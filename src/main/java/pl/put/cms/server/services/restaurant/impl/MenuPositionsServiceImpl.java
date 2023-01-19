0b6178baf6753e760dfad6bd2a295124a17ae24a
shaqu <pawel.lukaszewicz99@gmail.com>
2023-01-20 00:48:54 +0100

    MenuPositionsServiceImpl

---
+++ src/main/java/pl/put/cms/server/services/restaurant/impl/MenuPositionsServiceImpl.java
@@ -0,0 +1,43 @@
+package pl.put.cms.server.services.restaurant.impl;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.put.cms.server.entities.restaurant.MenuPosition;
+import pl.put.cms.server.entities.restaurant.dtos.MenuPositionDto;
+import pl.put.cms.server.repositories.restaurant.MenuPositionsRepository;
+import pl.put.cms.server.services.restaurant.MenuPositionsService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class MenuPositionsServiceImpl implements MenuPositionsService {
+
+    private final MenuPositionsRepository menuPositionsRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public MenuPositionsServiceImpl(MenuPositionsRepository menuPositionsRepository) {
+        this.menuPositionsRepository = menuPositionsRepository;
+        this.modelMapper = new ModelMapper();
+    }
+
+    @Override
+    public List<MenuPositionDto> getMenuPositionsShownOnMainPage() {
+        return menuPositionsRepository.getMenuPositionsByShownOnMainPageIsTrue()
+                .stream().map(this::mapMenuPositionToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<MenuPositionDto> getMenuPositionsByCategory(String category) {
+        return menuPositionsRepository.getMenuPositionsByCategory(category)
+                .stream().map(this::mapMenuPositionToDto)
+                .collect(Collectors.toList());
+    }
+
+    private MenuPositionDto mapMenuPositionToDto(MenuPosition menuPosition) {
+        return modelMapper.map(menuPosition, MenuPositionDto.class);
+    }
+}
