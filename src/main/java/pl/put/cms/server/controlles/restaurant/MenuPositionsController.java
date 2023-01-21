8d7c9f25631bc7e627434d1c46f93037b6ab0b57
shaqu <pawel.lukaszewicz99@gmail.com>
2023-01-21 15:02:33 +0100

    MenuPositionsController

---
+++ src/main/java/pl/put/cms/server/controlles/restaurant/MenuPositionsController.java
@@ -0,0 +1,40 @@
+package pl.put.cms.server.controlles.restaurant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import pl.put.cms.server.entities.restaurant.dtos.MenuPositionDto;
+import pl.put.cms.server.entities.restaurant.dtos.MenuPositionPictureDto;
+import pl.put.cms.server.services.restaurant.MenuPositionPictureService;
+import pl.put.cms.server.services.restaurant.MenuPositionsService;
+
+import java.util.List;
+
+@RestController("/menu")
+public class MenuPositionsController {
+
+    private final MenuPositionsService menuPositionsService;
+    private final MenuPositionPictureService menuPositionPictureService;
+
+    @Autowired
+    public MenuPositionsController(MenuPositionsService menuPositionsService, MenuPositionPictureService menuPositionPictureService) {
+        this.menuPositionsService = menuPositionsService;
+        this.menuPositionPictureService = menuPositionPictureService;
+    }
+
+    @GetMapping("/{restaurantId}/main-page")
+    public List<MenuPositionDto> getMenuPositionsShownOnMainPage(@PathVariable int restaurantId){
+        return menuPositionsService.getMenuPositionsShownOnMainPage(restaurantId);
+    }
+
+    @GetMapping("/{restaurantId}/menu/category/{category}")
+    public List<MenuPositionDto> getMenuPositionsByCategory(@PathVariable int restaurantId, @PathVariable String category) {
+        return menuPositionsService.getMenuPositionsByCategory(category, restaurantId);
+    }
+
+    @GetMapping("/menu/picture/{menuPositionId}")
+    public MenuPositionPictureDto getMenuPositionsByCategory(@PathVariable int menuPositionId) {
+        return menuPositionPictureService.getPicture(menuPositionId);
+    }
+}
