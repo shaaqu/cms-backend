b794a2a8d6cf898fba96f67a980ab666d8e6f6c4
shaqu <pawel.lukaszewicz99@gmail.com>
2023-02-08 19:04:48 +0100

    Add security config.

---
+++ src/main/java/pl/put/cms/server/configuration/SecurityConfig.java
@@ -0,0 +1,25 @@
+package pl.put.cms.server.configuration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll();
+        return http.build();
+    }
+
+}
