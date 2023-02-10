008f89f563bd2699fdbd7f67258baa8dc86f0891
Pawel Lukaszewicz <pawel.lukaszewicz@gft.com>
2023-02-10 19:13:14 +0100

    RestaurantLocation service impl.

--- src/main/java/pl/put/cms/server/services/restaurant/impl/RestaurantInfoServiceImpl.java
+++ src/main/java/pl/put/cms/server/services/restaurant/impl/RestaurantInfoServiceImpl.java
@@ -2,24 +2,28 @@ package pl.put.cms.server.services.restaurant.impl;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.annotation.AccessType;
 import org.springframework.stereotype.Service;
+import pl.put.cms.server.entities.restaurant.RestaurantLocation;
 import pl.put.cms.server.entities.restaurant.dtos.LocationDto;
 import pl.put.cms.server.entities.restaurant.dtos.RestaurantDto;
+import pl.put.cms.server.repositories.restaurant.RestaurantLocationRepository;
 import pl.put.cms.server.repositories.restaurant.RestaurantRepository;
 import pl.put.cms.server.services.restaurant.RestaurantInfoService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RestaurantInfoServiceImpl implements RestaurantInfoService {
 
     private final RestaurantRepository restaurantRepository;
+    private final RestaurantLocationRepository restaurantLocationRepository;
     private final ModelMapper modelMapper;
 
     @Autowired
-    public RestaurantInfoServiceImpl(RestaurantRepository restaurantRepository) {
+    public RestaurantInfoServiceImpl(RestaurantRepository restaurantRepository, RestaurantLocationRepository restaurantLocationRepository) {
         this.restaurantRepository = restaurantRepository;
+        this.restaurantLocationRepository = restaurantLocationRepository;
         this.modelMapper = new ModelMapper();
     }
 
@@ -31,6 +35,12 @@ public class RestaurantInfoServiceImpl implements RestaurantInfoService {
 
     @Override
     public List<LocationDto> getRestaurantLocations(int restaurantId) {
-        return null;
+        return restaurantLocationRepository.findAllByRestaurant_Id(restaurantId)
+                .stream().map(this::mapRestaurantLocationToDto)
+                .collect(Collectors.toList());
+    }
+
+    private LocationDto mapRestaurantLocationToDto(RestaurantLocation restaurantLocation) {
+        return modelMapper.map(restaurantLocation, LocationDto.class);
     }
 }
