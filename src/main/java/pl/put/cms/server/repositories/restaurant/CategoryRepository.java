f2087c48472b99192cd24ced32674d813e6fba5f
shaqu <pawel.lukaszewicz99@gmail.com>
2023-02-18 01:48:06 +0100

    Swagger works

--- src/main/java/pl/put/cms/server/repositories/restaurant/CategoryRepository.java
+++ src/main/java/pl/put/cms/server/repositories/restaurant/CategoryRepository.java
@@ -1,13 +1,12 @@
 package pl.put.cms.server.repositories.restaurant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.Repository;
 import pl.put.cms.server.entities.restaurant.Category;
 
 import java.util.List;
 
 
-public interface CategoryRepository extends JpaRepository<Repository, Integer> {
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
     List<Category> findAllByRestaurant_Id(int restaurantId);
 }
